923db39b2a0875a4f6f328836272713f5f0b615b
cuongpm <cuongphammanh@chanelvn.net>
2022-10-15 17:01:52 +0700

    feat: add mail config

---
+++ src/main/java/vn/ptit/config/MailConfig.java
@@ -0,0 +1,39 @@
+package vn.ptit.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${mail.name}")
+    private String nameMail;
+    @Value("${mail.password}")
+    private String passwordMail;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(nameMail);
+        mailSender.setPassword(passwordMail);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        return mailSender;
+    }
+
+}
