36e8aaaddf24e4049897bd60153e8e5ca5e53b36
cuongpm <cuongphammanh@chanelvn.net>
2022-10-15 17:15:04 +0700

    feat: reafactor laptop same

--- src/main/java/vn/ptit/controller/laptop/GetLaptopController.java
+++ src/main/java/vn/ptit/controller/laptop/GetLaptopController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.ptit.controller.ResponseBody;
+import vn.ptit.exception.InvalidDataException;
 import vn.ptit.exception.InvalidRequestException;
 import vn.ptit.model.PagingPayload;
 import vn.ptit.service.laptop.CreateLaptopService;
@@ -22,49 +23,60 @@ public class GetLaptopController {
     }
 
     @GetMapping("/detail/{id}")
-    public ResponseEntity<?> getDetail(@PathVariable("id") Long id){
+    public ResponseEntity<?> getDetail(@PathVariable("id") Long id) {
         if (id == null)
             throw new InvalidRequestException("Require [id]");
 
         PagingPayload.PagingPayloadBuilder payloadBuilder = PagingPayload.builder();
         payloadBuilder.timestamp(System.currentTimeMillis());
         payloadBuilder.data(Collections.singletonList(getLaptopService.getById(id)));
-        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(),ResponseBody.Status.SUCCESS,ResponseBody.Code.SUCCESS), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(), ResponseBody.Status.SUCCESS, ResponseBody.Code.SUCCESS), HttpStatus.OK);
     }
 
     @GetMapping("/list")
     public ResponseEntity<?> list(@RequestParam(value = "limit", required = false, defaultValue = "10") Integer limit,
                                   @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
-                                  @RequestParam(value = "sort", required = false, defaultValue = "asc") String sort){
+                                  @RequestParam(value = "sort", required = false, defaultValue = "asc") String sort) {
         PagingPayload.PagingPayloadBuilder payloadBuilder = PagingPayload.builder();
         payloadBuilder.timestamp(System.currentTimeMillis());
-        payloadBuilder.data(getLaptopService.getList(page,limit, sort));
-        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(),ResponseBody.Status.SUCCESS,ResponseBody.Code.SUCCESS), HttpStatus.OK);
+        payloadBuilder.data(getLaptopService.getList(page, limit, sort));
+        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(), ResponseBody.Status.SUCCESS, ResponseBody.Code.SUCCESS), HttpStatus.OK);
     }
 
-    @GetMapping("/get-same-manufacturer")
-    public ResponseEntity<?> list(@RequestParam(value = "manufacturer-id") Long manufacturerId,
-                                  @RequestParam(value = "laptop-id") Long id,
-                                  @RequestParam(value = "limit", required = false, defaultValue = "4") Integer limit){
+    @GetMapping("/same")
+    public ResponseEntity<?> list(@RequestHeader(name = "manufacturer-id") Long manufacturerId,
+                                  @RequestHeader(name = "laptop-id") Long id,
+                                  @RequestParam(value = "limit", required = false, defaultValue = "4") Integer limit) {
+        if (manufacturerId == null)
+            throw new InvalidRequestException("Require [manufacturer-id]");
+
+        if (id == null)
+            throw new InvalidRequestException("Require [laptop-id]");
+
+        if (limit != null && limit < 0) {
+            throw new InvalidRequestException("QueryFilter[limit] must >= 0");
+        }
+
         PagingPayload.PagingPayloadBuilder payloadBuilder = PagingPayload.builder();
         payloadBuilder.timestamp(System.currentTimeMillis());
         payloadBuilder.data(getLaptopService.getSameManufacturer(manufacturerId, id, limit));
-        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(),ResponseBody.Status.SUCCESS,ResponseBody.Code.SUCCESS), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(), ResponseBody.Status.SUCCESS, ResponseBody.Code.SUCCESS), HttpStatus.OK);
     }
+
     @GetMapping("/filter")
     public ResponseEntity<?> filter(@RequestParam(value = "limit", required = false, defaultValue = "10") Integer limit,
-                                  @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
-                                  @RequestParam(value = "sort", required = false, defaultValue = "asc") String sort,
+                                    @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+                                    @RequestParam(value = "sort", required = false, defaultValue = "asc") String sort,
                                     @RequestParam(value = "key", required = false) String searchText,
                                     @RequestParam(value = "manufacturer", required = false) List<Long> manufacturerIds,
                                     @RequestParam(value = "category", required = false) List<Integer> categories,
                                     @RequestParam(value = "cpu", required = false) List<String> cpus,
                                     @RequestParam(value = "ram", required = false) List<String> rams,
                                     @RequestParam(value = "hard-drive", required = false) List<String> hardDrives,
-                                    @RequestParam(value = "vga", required = false) List<String> vgas){
+                                    @RequestParam(value = "vga", required = false) List<String> vgas) {
         PagingPayload.PagingPayloadBuilder payloadBuilder = PagingPayload.builder();
         payloadBuilder.timestamp(System.currentTimeMillis());
         payloadBuilder.data(getLaptopService.filter(page, limit, sort, searchText, manufacturerIds, categories, cpus, rams, hardDrives, vgas));
-        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(),ResponseBody.Status.SUCCESS,ResponseBody.Code.SUCCESS), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseBody(payloadBuilder.build(), ResponseBody.Status.SUCCESS, ResponseBody.Code.SUCCESS), HttpStatus.OK);
     }
 }
