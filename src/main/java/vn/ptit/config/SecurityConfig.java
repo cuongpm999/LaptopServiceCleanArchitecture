1d1c31a585531c2983725dacbd9b350af41e62d1
cuongpm <cuongphammanh@chanelvn.net>
2022-10-25 23:32:24 +0700

    feat: add cors sercurity

--- src/main/java/vn/ptit/config/SecurityConfig.java
+++ src/main/java/vn/ptit/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint)
